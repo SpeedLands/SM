e7c37c30d6ae975f6fd8a902cebc16f40353d0b7
Quetzal201 <pedroramirezsuarez100@gmail.com>
2026-01-14 18:38:04 -0600

    Permite la conexion a tunnel. Se modifico el archivo para forzar a Laravel a cargar archivos js y css con https, que es requerido por cloudflared para cargar diseño.

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    protected $proxies;
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+    public function __construct()
+    {
+        $this->proxies = Config::get('trustedproxy.proxies', '*');
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Enforce HTTPS
+        if (config('app.url') && str_starts_with(config('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\Gate::define('admin-only', function ($user) {
             return $user->role === 'ADMIN';
         });
